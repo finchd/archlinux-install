ea10c3cf0aa74d840e0f0fd269699ca87fc922c3
Donovan Finch <finchd@users.noreply.github.com>
2018-03-24 13:38:52 -0700

    Verified steps in VM
    Checked steps in a Hyper-V VM, both "stage 1" and "stage 2".
    Next steps are the VFIO section - need HW to confirm PCI lane group assignments on my motherboard are compatible.

--- archlinux-install.sh
+++ archlinux-install.sh
@@ -10,10 +10,17 @@ timedatectl set-ntp true
 echo "Updating installer's set of packager's trusted GPG Keys"
 pacman-key --refresh-keys
 
-pacstrap /mnt base base-devel openssh qemu libvirt omvf virt-manager
+pacstrap /mnt base base-devel grub os-prober lsb-release openssh libvirt vim
 #python2 for ansible 
+#grub 2 / os-prober to boot with, os-prober used by default in /etc/grub.d/30_os-prober
+#lsb-release improves detection from other OSes, makes /etc/lsb-release
 #openssh or #openssh-server 
-# qemu, libvirt, ovmf, and virt-manager (gui?) for VFIO
+# libvirt, (qemu, ovmf, and virt-manager gui) for VFIO
+#vim and vim-plugins group?
+
+#make fstab
+echo "Making fstab"
+genfstab -U /mnt >> /mnt/etc/fstab
 
 #Copy second bootstrapping script for post-chroot
 echo "Copying /tmp/arch-stage2.sh to /mnt/opt/arch-stage2.sh"
